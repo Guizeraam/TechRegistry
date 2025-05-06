0247048ce8748d2b16c9119bc183323654bde763
Guilherme Ferraz <guilherme.ferraz@LAJESPATAGONIA.INT>
2025-05-06 18:02:03 -0300

    listando banco

--- home.php
+++ home.php
@@ -30,35 +30,18 @@ $result = mysqli_query($conecta, $query);
         <h3 class="nome-direita">Bem-vindo, <?php echo $_SESSION['usuario']; ?>!</h3>
     </div>
         <div class="conteudo">
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                     </tr>
+                </thead>
+            <tbody>
+            </tbody>
+            </table>
         <!-- //    if (mysqli_num_rows($result) > 0) {
         //     // Iterar pelos resultados
         //     echo "<table>";
